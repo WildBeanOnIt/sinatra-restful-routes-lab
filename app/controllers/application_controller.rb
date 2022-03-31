e291a13a22df297a5217e9e44c4fb34f7f464e8d
Julio Aguilera Jr <julioaguilera337@gmail.com>
2022-03-30 21:23:53 -0500

    created files for controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,23 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
+  get '/recipes' do
+    # binding.pry
+    @recipes = Recipe.all
+    erb :index
+  end
 
+  get '/recipes/new' do 
+    erb :new
+  end
+
+  post '/recipes' do
+    @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
+    render to "/recipes/#{@recipe.id}"
+  end
+
+  get "/recipes/:id" do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :show
+  end
 end
