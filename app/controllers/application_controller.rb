f6b4a42eee247a1bd45f22292031fc38bdb4e1ac
Julio Aguilera Jr <julioaguilera337@gmail.com>
2022-04-05 20:56:51 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,12 +5,14 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
+  # index
   get '/recipes' do
     # binding.pry
     @recipes = Recipe.all
     erb :index
   end
 
+  # new
   get '/recipes/new' do 
     erb :new
   end
@@ -19,9 +21,19 @@ class ApplicationController < Sinatra::Base
     @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
     render to "/recipes/#{@recipe.id}"
   end
-
+  
+  #show
   get "/recipes/:id" do
     @recipe = Recipe.find_by_id(params[:id])
     erb :show
   end
+  
+  # delete
+  delete "/recipe/:id" do
+    Recipe.destroy(params[:id])
+    redirect to "/recipes"
+  end
+
+
 end
+
